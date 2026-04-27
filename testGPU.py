862c354c9505404b07ec52a3a22979cc237a3bd5
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-26 21:24:55 -0400

    add testGPU

---
+++ testGPU.py
@@ -0,0 +1,9 @@
+import torch
+
+print("PyTorch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    print("GPU name:", torch.cuda.get_device_name(0))
+else:
+    print("No GPU detected or CUDA not installed correctly.")
