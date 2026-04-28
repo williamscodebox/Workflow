1aef46e12d0015769fc7dc3b2655911f0434efd1
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-27 22:11:01 -0400

    update subtitle

--- video-creator/models/subtitle.py
+++ video-creator/models/subtitle.py
@@ -3,7 +3,7 @@ import json
 import os
 import tempfile
 
-WHISPERX_PYTHON = r"C:\Users\panda\PycharmProjects\Workflow\whisperXenv\Scripts\python.exe"
+WHISPERX_PYTHON = r"C:\Users\panda\PycharmProjects\Workflow\.whisperXvenv\Scripts\python.exe"
 
 class SubtitleModel:
     # noinspection PyMethodMayBeStatic
@@ -13,23 +13,32 @@ class SubtitleModel:
         """
 
         # Temporary output file
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as tmp:
-            output_json = tmp.name
+        # with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as tmp:
+        #     output_json = tmp.name
+        with tempfile.TemporaryDirectory() as tmpdir:
 
-        # Run WhisperX
-        cmd = [
-            WHISPERX_PYTHON,
-            "-m", "whisperx",
-            audio_path,
-            "--model", "large-v3",
-            "--output_format", "json",
-            "--output_file", output_json
-        ]
+          # Run WhisperX
+          cmd = [
+              WHISPERX_PYTHON,
+              "-m", "whisperx",
+              audio_path,
+              "--model", "large-v3",
+              "--output_format", "json",
+              "--output_dir", tmpdir,
+              "--vad_method", "silero",
+              "--no_align"
+          ]
+
+        # print("WHISPERX CMD:", cmd)
 
         subprocess.run(cmd, check=True)
 
+        # WhisperX always names the file <audio>.json
+        base = os.path.splitext(os.path.basename(audio_path))[0]
+        json_path = os.path.join(tmpdir, f"{base}.json")
+
         # Load WhisperX output
-        with open(output_json, "r", encoding="utf-8") as f:
+        with open(json_path, "r", encoding="utf-8") as f:
             data = json.load(f)
 
         # Convert to your expected format
